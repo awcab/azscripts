9c811a05c2e971639edb29950bf67b7774bda328
Adam Cabler <awcab@users.noreply.github.com>
2019-01-15 21:24:56 -0800

    added second mount

--- azmount.ps1
+++ azmount.ps1
@@ -2,11 +2,17 @@ param(
   [Parameter(Mandatory=$true)] [string]$key,
   [Parameter(Mandatory=$true)] [string]$mountName,
   [Parameter(Mandatory=$true)] [string]$drive,
-  [Parameter(Mandatory=$true)] [string]$root
+  [Parameter(Mandatory=$true)] [string]$origdrive,
+  [Parameter(Mandatory=$true)] [string]$root,
+  [Parameter(Mandatory=$true)] [string]$origroot
 )
 
 $acctKey = ConvertTo-SecureString -String $key -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential -ArgumentList $mountName, $acctKey
+
 New-PSDrive -Name $drive -PSProvider FileSystem -Root $root -Credential $credential -Persist
+
+New-PSDrive -Name $origdrive -PSProvider FileSystem -Root $origroot -Credential $credential -Persist
+
 
 
